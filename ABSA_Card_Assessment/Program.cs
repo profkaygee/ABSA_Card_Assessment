fc87fa7655e3f48a680600d3a4deeabf0e9aa041
KAGISOCV-NB\kagis <profkagiso@gmail.com>
2022-03-31 10:39:26 +0200

    Add project files.

---
+++ ABSA_Card_Assessment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABSA_Card_Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
